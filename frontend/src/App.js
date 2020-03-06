18bcfbddc527560d53a9728bb1583cb0b1fc4b76
bush-man92 <42248918+bush-man92@users.noreply.github.com>
2020-03-06 21:50:44 +0100

    select and deselect meal, calculation of total price

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import Grid from '@material-ui/core/Grid';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Paper from "@material-ui/core/Paper";
-import { Container, Button } from "@material-ui/core";
+import { Container, Button, Select, MenuItem } from "@material-ui/core";
+import update from 'immutability-helper';
 
 import './App.css';
 
@@ -13,8 +14,13 @@ class App extends Component {
       data: {},
       labels: [],
       meals: [],
-      persons: [{person1: "None"},{person2: "None"}],
-      selected: { meal: "None", drink: "None"}
+      showed_meals: [],
+      selected_tag: "None",
+      selected_drink: { meal: "None", drink: "None"},
+      selected_person: "person1",
+      selected_meal: {},
+      meals_list: [],
+      total_price: 0,
     };
   }
 
@@ -22,10 +28,63 @@ class App extends Component {
     this.state.meals.forEach(meal => {
       if (meal.id === id) {
         this.setState({
-          selected: {meal: meal, drink: drink}
+          selected_drink: {meal: meal, drink: drink}
         })
       }
     });
+  }
+
+  change_person = async event => {
+    await this.setState({
+      selected_person: event.target.value
+    })
+    this.set_total_price()
+  }
+
+  change_tag = async (tag) => {
+    await this.setState({
+      selected_tag: tag
+    })
+  }
+
+  set_meal = async (meal) => {
+    var drink = "None"
+
+    if (this.state.selected_drink.meal.id === meal.id) {
+      drink = this.state.selected_drink.drink
+    }
+
+    this.setState({
+      selected_meal: {person: this.state.selected_person, meal: meal, drink: drink,}
+    })
+
+    await this.setState(state => {
+      const meals_list = state.meals_list.concat(state.selected_meal)
+      return {meals_list}
+    })
+    this.set_total_price()
+}
+
+  set_total_price = () => {
+    var total_price = 0
+
+    this.state.meals_list.forEach(item => {
+      if (this.state.selected_person === item.person) {
+        total_price = total_price + item.meal.price + item.drink.price
+      }
+    })
+    
+    this.setState({
+      total_price: total_price.toFixed(2)
+    })
+  }
+
+  deselect = async (id) => {
+    await this.setState(state => {
+      const meals_list = state.meals_list.filter((item, index) => id !== index);
+      return {meals_list}
+    });
+    this.set_total_price()
   }
 
   async componentDidMount() {
@@ -57,12 +116,31 @@ class App extends Component {
               <Grid item xs={8}>
                 <Paper class="paper">
                   {this.state.labels.map(value => (
-                    <Button class="primary" key={value.id}>{value.label}</Button>
+                    <Button style={{margin:10}} variant="outlined" onClick={() => this.deselect(value.label)} key={value.id}>{value.label}</Button>
                   ))}
                 </Paper>
               </Grid>
               <Grid item xs={4}>
-                <Paper class="paper">Persons</Paper>
+                <Paper class="paper">
+                  <p>SELECT PERSON</p>
+                  <Select
+                    value={this.state.selected_person}
+                    onChange={this.change_person}
+                   >
+                  <MenuItem value={"person1"}>Person1</MenuItem>
+                  <MenuItem value={"person2"}>Person2</MenuItem>
+                </Select>
+                {this.state.meals_list.map((meal, index) => (
+                    <Container>
+                      {this.state.selected_person === meal.person ? 
+                        <p>{meal.meal.title}: {meal.meal.price}$,
+                          {meal.drink.title}: {meal.drink.price}$
+                          <Button key={index} onClick={() => this.deselect(index)} variant="outlined">Deselect</Button></p>: <p></p>
+                      }
+                    </Container>
+                  ))}
+                  <p>Total price: {this.state.total_price}</p>
+                </Paper>
               </Grid>
               {this.state.meals.map(meal => (
                 <Grid key={meal.id} item xs={8}>
@@ -73,18 +151,18 @@ class App extends Component {
                       </Grid>
                       <Grid item xs={8}>
                         <p>{meal.title} + drink</p>
-                        <p>{meal.starter}</p>
-                        <p>{meal.desert}</p>
-                        <p>Selected drink: {this.state.selected.meal.id === meal.id ? this.state.selected.drink.title : "None"}</p>
+                        <p>Starter: {meal.starter}</p>
+                        <p>Desert: {meal.desert}</p>
+                        <p>Selected drink: {this.state.selected_drink.meal.id === meal.id ? this.state.selected_drink.drink.title : "None"}</p>
                         <Grid container>
                           <Grid item xs={6}>
                             {meal.drinks.map(drink => (
-                              <Button key={drink.id} onClick={() => this.select_drink(meal.id, drink)}>{drink.title}</Button>
+                              <Button style={{margin:5}} variant="outlined" key={drink.id} onClick={() => this.select_drink(meal.id, drink)}>{drink.title}</Button>
                             ))}
                           </Grid>
                           <Grid item xs={4}>
-                            <p>PRICE: {meal.price}</p>
-                            <Button>Select</Button>
+                            <p>PRICE: {this.state.selected_drink.meal.id === meal.id ? (meal.price + this.state.selected_drink.drink.price).toFixed(2) : meal.price}</p>
+                            <Button variant="outlined" onClick={() => this.set_meal(meal)}>Select</Button>
                           </Grid>
                         </Grid>
                       </Grid>
